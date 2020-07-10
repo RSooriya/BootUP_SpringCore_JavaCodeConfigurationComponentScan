c10d4f30a34783cd0fc0989c2dca146f82b32538
RSooriya <soormo06@gmail.com>
2020-07-10 18:19:56 +0530

    Comments added

--- src/com/sooriya/MyApp.java
+++ src/com/sooriya/MyApp.java
@@ -6,10 +6,15 @@ public class MyApp {
 
 	public static void main(String[] args)
 	{
+		
+		// Since we are configuring using pure java code - AnnotationConfigApplicationContext
+		//creating spring container and loading configuration file
 		AnnotationConfigApplicationContext context = new
 				AnnotationConfigApplicationContext(SportConfig.class);
+		// retrieving beans from the container
 		TennisCoach theCoach = context.getBean("tennisCoach",TennisCoach.class);
 		Coach alphaCoach = context.getBean("tennisCoach",Coach.class);
+		//calling methods on bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
 		System.out.println(theCoach.getEmail());
--- src/com/sooriya/SportConfig.java
+++ src/com/sooriya/SportConfig.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+//java code configuration instead of xml configuration 
 @Configuration
-@ComponentScan("com.sooriya")
-@PropertySource("classpath:sport.properties")
+@ComponentScan("com.sooriya")   // to scan classes
+@PropertySource("classpath:sport.properties")   // to retrieve values from property file
 public class SportConfig {
 
 }
